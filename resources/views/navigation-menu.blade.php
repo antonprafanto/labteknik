39fbffba5f1eac0400b9217cadf32d10b28644d0
Anton Prafanto <antonprafanto@gmail.com>
2026-01-30 10:12:15 +0800

    feat: Add Website link to navbar for logged-in users

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -18,6 +18,10 @@
                 </x-nav-link>
                 @endif
                 
+                <x-nav-link href="{{ url('/') }}" :active="false">
+                    {{ __('Website') }}
+                </x-nav-link>
+                
                 <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
                 </x-nav-link>
@@ -285,6 +289,9 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link href="{{ url('/') }}" :active="false">
+                {{ __('Website') }}
+            </x-responsive-nav-link>
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
